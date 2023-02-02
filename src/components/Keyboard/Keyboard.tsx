99d0ecbc9b62d4065f3edc98d52589e9692b6281
Carles Galan <galan.carles@gmail.com>
2023-02-02 22:32:02 +0100

    Add clear all numbers utility

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -4,12 +4,12 @@ import Key from "../Key/Key";
 import "./Keyboard.css";
 
 const Keyboard = (): JSX.Element => {
-  const { addNumber } = useContext(PhoneContext);
+  const { modifyNumber } = useContext(PhoneContext);
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "delete"];
   return (
     <ol className="keyboard">
       {numbers.map((listItem) => (
-        <Key number={listItem} action={addNumber} />
+        <Key number={listItem} action={modifyNumber} />
       ))}
     </ol>
   );
